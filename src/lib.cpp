b4c5b1f942cfe11d19b5cca386834d4681e6b5e1
Shawn Zhong <github@shawnzhong.com>
2021-10-12 19:54:40 -0500

    Update .clang-format

--- src/lib.cpp
+++ src/lib.cpp
@@ -1,15 +1,16 @@
-#include <cstdio>
 #include <dlfcn.h>
 #include <unistd.h>
 
+#include <cstdio>
+
 namespace ulayfs {
 namespace posix {
-#define REGISTER_FN(name)                                                      \
+#define REGISTER_FN(name) \
   auto name = reinterpret_cast<decltype(&::name)>(dlsym(RTLD_NEXT, #name))
 
 REGISTER_FN(write);
 REGISTER_FN(read);
-} // namespace posix
+}  // namespace posix
 
 extern "C" {
 ssize_t write(int fd, const void *buf, size_t count) {
@@ -22,4 +23,4 @@ ssize_t read(int fd, void *buf, size_t count) {
   return posix::write(fd, buf, count);
 }
 }
-} // namespace ulayfs
+}  // namespace ulayfs
