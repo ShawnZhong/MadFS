acc807b0cc4cbb1adf68813c5bc2441728ba055c
Chenhao Ye <ye.chenhao.ych@gmail.com>
2021-10-14 22:16:09 -0500

    add some basic layout operations

--- src/layout.h
+++ src/layout.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <atomic>
+#include <bit>
 #include <cstdint>
 
 #include "config.h"
@@ -12,15 +14,20 @@ using Bitmap = uint64_t;
 class LogEntry;
 
 class TxEntry {
-  uint64_t tx_entry;
+ public:
+  uint64_t entry;
 };
 
-class TxBegin : TxEntry {};
+class TxBeginEntry : public TxEntry {};
 
-class TxCommit : TxEntry {};
+class TxCommitEntry : public TxEntry {};
+
+enum LogOp : uint32_t {
+  LOG_OVERWRITE,
+};
 
 class LogEntry {
-  uint32_t op;
+  enum LogOp op;
   BlockIdx file_offset;
   BlockIdx block_offset;
   uint32_t size;
@@ -34,7 +41,7 @@ constexpr static int NUM_INLINE_BITMAP = 24;
 constexpr static int NUM_INLINE_TX_ENTRY = 480;
 
 /*
- * Idx: 0          1          2          3
+ * Idx: 0          1          2
  * +----------+----------+----------+----------+----------+----------+----------
  * |   Meta   | Bitmap 1 | Bitmap 2 |   ...    |   ...    | Data/Log |   ...
  * +----------+----------+----------+----------+----------+----------+----------
@@ -69,13 +76,59 @@ class MetaBlock {
 };
 
 class BitmapBlock {
+  constexpr static uint64_t BITMAP_ALL_USED = 0xffffffffffffffff;
+
   Bitmap bitmaps[NUM_BITMAP];
+
+  // allocate one block; return the index of allocated block
+  // accept a hint for which bit to start searching
+  // usually hint can just be the last idx return by this function
+  int alloc(int hint = 0) {
+    for (int idx = (hint >> 6); idx < NUM_BITMAP; ++idx) {
+    retry:
+      Bitmap b = __atomic_load_n(&bitmaps[idx], __ATOMIC_ACQUIRE);
+      if (b == BITMAP_ALL_USED) continue;
+      Bitmap allocated = ~b & (b + 1);
+      if (!__atomic_compare_exchange_n(&bitmaps[idx], &b, b & allocated, true,
+                                       __ATOMIC_ACQ_REL, __ATOMIC_ACQUIRE))
+        goto retry;
+      return (idx << 6) + std::countr_zero(b);
+    }
+    return -1;
+  }
+
+  // 64 blocks are considered one batch; return the index of the first block
+  int alloc_batch(int hint = 0) {
+    for (int idx = (hint >> 6); idx < NUM_BITMAP; ++idx) {
+      uint64_t expected = 0;
+      if (__atomic_load_n(&bitmaps[idx], __ATOMIC_ACQUIRE) != 0) continue;
+      if (!__atomic_compare_exchange_n(&bitmaps[idx], &expected,
+                                       BITMAP_ALL_USED, false, __ATOMIC_ACQ_REL,
+                                       __ATOMIC_ACQUIRE))
+        continue;
+      return (idx << 6);
+    }
+    return -1;
+  }
 };
 
 class TxLogBlock {
   BlockIdx prev;
   BlockIdx next;
   TxEntry tx_entries[NUM_TX_ENTRY];
+
+ public:
+  int try_commit(TxCommitEntry commit_entry, uint32_t hint_tail = 0) {
+    for (auto idx = hint_tail; idx < NUM_LOG_ENTRY; ++idx) {
+      uint64_t expected = 0;
+      if (__atomic_load_n(&tx_entries[idx].entry, __ATOMIC_ACQUIRE)) continue;
+      if (__atomic_compare_exchange_n(&tx_entries[idx].entry, &expected,
+                                      commit_entry.entry, false,
+                                      __ATOMIC_RELEASE, __ATOMIC_ACQUIRE))
+        return idx;
+    }
+    return -1;
+  }
 };
 
 class LogEntryBlock {
