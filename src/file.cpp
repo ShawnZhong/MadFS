1085c8662a750b2b0a1c42585147870f183882cf
Shawn Zhong <github@shawnzhong.com>
2021-10-19 00:17:37 -0500

    Bugfix for the `is_create` case

--- src/file.cpp
+++ src/file.cpp
@@ -4,7 +4,6 @@ namespace ulayfs::dram {
 
 int File::open(const char* pathname, int flags, mode_t mode) {
   int ret;
-  bool is_create = false;
   fd = posix::open(pathname, flags, mode);
   if (fd < 0) return fd;  // fail to open the file
   open_flags = flags;
@@ -12,17 +11,21 @@ int File::open(const char* pathname, int flags, mode_t mode) {
   struct stat stat_buf;
   ret = posix::fstat(fd, &stat_buf);
   if (ret) throw std::runtime_error("Fail to fstat!");
-  is_create = stat_buf.st_size == 0;
 
-  if (is_create) {
+  bool should_create = stat_buf.st_size == 0;
+  if (should_create) {
     ret = posix::ftruncate(fd, LayoutOptions::prealloc_size);
     if (ret) throw std::runtime_error("Fail to ftruncate!");
+
+    // call fstat again to get the latest file size
+    ret = posix::fstat(fd, &stat_buf);
+    if (ret) throw std::runtime_error("Fail to fstat!");
   }
 
   meta = mtable.init(fd, stat_buf.st_size);
   allocator.init(fd, meta, &mtable);
 
-  if (is_create) meta->init();
+  if (should_create) meta->init();
   return fd;
 }
 
