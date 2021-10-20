906599d307d668d98ef302c9606e4f81dca10e43
Shawn Zhong <github@shawnzhong.com>
2021-10-20 02:47:55 -0500

    Add show config
    Update config.h
    
    Update lib.cpp

--- src/config.h
+++ src/config.h
@@ -1,44 +1,67 @@
 #pragma once
 
+#include <ostream>
 namespace ulayfs {
 
-namespace BuildOptions {
+constexpr static struct BuildOptions {
+  constexpr static const char* git_branch = GIT_BRANCH;
+  constexpr static const char* git_commit_hash = GIT_COMMIT_HASH;
+
 #ifdef MEM_PROTECT
-constexpr static bool mem_protect = true;
+  constexpr static bool mem_protect = true;
 #else
-constexpr static bool mem_protect = false;
+  constexpr static bool mem_protect = false;
 #endif
 
 #ifdef RELAXED
-constexpr static bool relaxed = true;
+  constexpr static bool relaxed = true;
 #else
-constexpr static bool relaxed = false;
+  constexpr static bool relaxed = false;
 #endif
 
 #ifdef NDEBUG
-constexpr static bool debug = false;
+  constexpr static bool debug = false;
 #else
-constexpr static bool debug = true;
+  constexpr static bool debug = true;
+#endif
+
+#ifdef NO_SHOW_CONFIG
+  constexpr static bool show_config = false;
+#else
+  constexpr static bool show_config = true;
 #endif
 
 #ifdef USE_HUGEPAGE
-constexpr static bool use_hugepage = true;
+  constexpr static bool use_hugepage = true;
 #else
-constexpr static bool use_hugepage = false;
+  constexpr static bool use_hugepage = false;
 #endif
 
 #ifdef __CLWB__
-constexpr static bool support_clwb = true;
+  constexpr static bool support_clwb = true;
 #else
-constexpr static bool support_clwb = false;
+  constexpr static bool support_clwb = false;
 #endif
 
 #ifdef __CLFLUSHOPT__
-constexpr static bool support_clflushopt = true;
+  constexpr static bool support_clflushopt = true;
 #else
-constexpr static bool support_clflushopt = false;
+  constexpr static bool support_clflushopt = false;
 #endif
-}  // namespace BuildOptions
+
+  friend std::ostream& operator<<(std::ostream& out, const BuildOptions& _) {
+    out << "BuildOptions: \n";
+    out << "\tgit_branch: " << git_branch << "\n";
+    out << "\tgit_commit_hash: " << git_commit_hash << "\n";
+    out << "\tmem_protect: " << mem_protect << "\n";
+    out << "\trelaxed: " << relaxed << "\n";
+    out << "\tdebug: " << debug << "\n";
+    out << "\tuse_hugepage: " << use_hugepage << "\n";
+    out << "\tsupport_clwb: " << support_clwb << "\n";
+    out << "\tsupport_clflushopt: " << support_clflushopt << "\n";
+    return out;
+  }
+} build_options;  // namespace BuildOptions
 
 struct RuntimeOptions {
   RuntimeOptions(){
--- src/lib.cpp
+++ src/lib.cpp
@@ -9,7 +9,7 @@
 
 namespace ulayfs {
 extern "C" {
-int open(const char *pathname, int flags, ...) {
+int open(const char* pathname, int flags, ...) {
   mode_t mode = 0;
 
   if (__OPEN_NEEDS_MODE(flags)) {
@@ -25,18 +25,32 @@ int open(const char *pathname, int flags, ...) {
   return fd;
 }
 
-ssize_t write(int fd, const void *buf, size_t count) {
+ssize_t write(int fd, const void* buf, size_t count) {
   if constexpr (BuildOptions::debug) {
     printf("write:count:%lu\n", count);
   }
   return posix::write(fd, buf, count);
 }
 
-ssize_t read(int fd, void *buf, size_t count) {
+ssize_t read(int fd, void* buf, size_t count) {
   if constexpr (BuildOptions::debug) {
     printf("read:count:%lu\n", count);
   }
   return posix::read(fd, buf, count);
 }
+
+/**
+ * Called when the shared library is loaded
+ */
+void __attribute__((constructor)) ulayfs_ctor() {
+  if constexpr (BuildOptions::show_config) {
+    std::cout << build_options << std::endl;
+  }
+}
+
+/**
+ * Called when the shared library is unloaded
+ */
+void __attribute__((destructor)) ulayfs_dtor() {}
 }  // extern "C"
 }  // namespace ulayfs
