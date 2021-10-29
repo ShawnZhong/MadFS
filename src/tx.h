41cce00e72805fdaece2bc4796a3af9ed10e7537
Chenhao Ye <ye.chenhao.ych@gmail.com>
2021-10-28 21:36:14 -0500

    finish ctor update

--- src/tx.h
+++ src/tx.h
@@ -16,13 +16,89 @@ class TxMgr {
   MemTable* mem_table;
 
   // the tail of the local tx entry
-  pmem::TxEntryIdx local_tx_tail{};
+  pmem::TxEntryIdx local_tx_tail;
 
   // the block for the local_tx_tail
-  pmem::TxLogBlock* local_tx_log_block{nullptr};
+  pmem::TxLogBlock* local_tx_log_block;
 
   // the tail of the local log entry
-  pmem::LogEntryIdx local_log_tail{};
+  pmem::LogEntryIdx local_log_tail;
+
+ public:
+  TxMgr() = default;
+  TxMgr(pmem::MetaBlock* meta, Allocator* allocator, MemTable* mem_table)
+      : meta(meta),
+        allocator(allocator),
+        mem_table(mem_table),
+        local_tx_tail(),
+        local_tx_log_block(nullptr),
+        local_log_tail() {}
+
+  /**
+   * Move to the next tx index
+   */
+  void forward_tx_idx() {
+    local_tx_tail = get_next_tx_idx(local_tx_tail, &local_tx_log_block);
+  }
+
+  /**
+   * @return the current transaction entry
+   */
+  [[nodiscard]] pmem::TxEntry get_curr_tx_entry() const {
+    return get_tx_entry(local_tx_tail, local_tx_log_block);
+  }
+
+  /**
+   * Begin a transaction that affects the range of blocks
+   * [start_virtual_idx, start_virtual_idx + num_blocks)
+   * @param start_virtual_idx
+   * @param num_blocks
+   */
+  pmem::TxEntryIdx begin_tx(VirtualBlockIdx start_virtual_idx,
+                            uint32_t num_blocks) {
+    pmem::TxBeginEntry tx_begin_entry{start_virtual_idx, num_blocks};
+    auto tx_log_tail = append_tx_begin_entry(tx_begin_entry);
+    meta->set_tx_log_tail(tx_log_tail);
+    return tx_log_tail;
+  }
+
+  pmem::TxEntryIdx commit_tx(pmem::TxEntryIdx tx_begin_idx,
+                             pmem::LogEntryIdx log_entry_idx) {
+    // TODO: compute begin_offset from tx_begin_idx
+    pmem::TxCommitEntry tx_commit_entry{0, log_entry_idx};
+    auto tx_log_tail = append_tx_commit_entry(tx_commit_entry);
+    meta->set_tx_log_tail(tx_log_tail);
+    return tx_log_tail;
+  }
+
+  pmem::LogEntryIdx write_log_entry(VirtualBlockIdx start_virtual_idx,
+                                    LogicalBlockIdx start_logical_idx,
+                                    uint8_t num_blocks,
+                                    uint16_t last_remaining) {
+    // prepare the log_entry
+    pmem::LogEntry log_entry;  // NOLINT(cppcoreguidelines-pro-type-member-init)
+    log_entry.op = pmem::LOG_OVERWRITE;
+    log_entry.last_remaining = last_remaining;
+    log_entry.num_blocks = num_blocks;
+    log_entry.next.block_idx = 0;
+    log_entry.next.local_idx = 0;
+    log_entry.start_virtual_idx = start_virtual_idx;
+    log_entry.start_logical_idx = start_logical_idx;
+
+    // check we need to allocate a new log entry block
+    if (local_log_tail.block_idx == 0 ||
+        local_log_tail.local_idx == NUM_LOG_ENTRY - 1) {
+      local_log_tail.block_idx = allocator->alloc(1);
+      local_log_tail.local_idx = 0;
+    }
+
+    // append the log entry
+    auto block = mem_table->get_addr(local_log_tail.block_idx);
+    auto log_entry_block = &block->log_entry_block;
+    log_entry_block->append(log_entry, local_log_tail.local_idx);
+
+    return local_log_tail;
+  }
 
   /**
    * Read the entry from the MetaBlock or TxLogBlock
@@ -148,77 +224,8 @@ class TxMgr {
     }
   };
 
+  // debug
  public:
-  TxMgr() = default;
-  TxMgr(pmem::MetaBlock* meta, Allocator* allocator, MemTable* mem_table)
-      : meta(meta), allocator(allocator), mem_table(mem_table) {}
-
-  /**
-   * Move to the next tx index
-   */
-  void forward_tx_idx() {
-    local_tx_tail = get_next_tx_idx(local_tx_tail, &local_tx_log_block);
-  }
-
-  /**
-   * @return the current transaction entry
-   */
-  [[nodiscard]] pmem::TxEntry get_curr_tx_entry() const {
-    return get_tx_entry(local_tx_tail, local_tx_log_block);
-  }
-
-  /**
-   * Begin a transaction that affects the range of blocks
-   * [start_virtual_idx, start_virtual_idx + num_blocks)
-   * @param start_virtual_idx
-   * @param num_blocks
-   */
-  pmem::TxEntryIdx begin_tx(VirtualBlockIdx start_virtual_idx,
-                            uint32_t num_blocks) {
-    pmem::TxBeginEntry tx_begin_entry{start_virtual_idx, num_blocks};
-    auto tx_log_tail = append_tx_begin_entry(tx_begin_entry);
-    meta->set_tx_log_tail(tx_log_tail);
-    return tx_log_tail;
-  }
-
-  pmem::TxEntryIdx commit_tx(pmem::TxEntryIdx tx_begin_idx,
-                             pmem::LogEntryIdx log_entry_idx) {
-    // TODO: compute begin_offset from tx_begin_idx
-    pmem::TxCommitEntry tx_commit_entry{0, log_entry_idx};
-    auto tx_log_tail = append_tx_commit_entry(tx_commit_entry);
-    meta->set_tx_log_tail(tx_log_tail);
-    return tx_log_tail;
-  }
-
-  pmem::LogEntryIdx write_log_entry(VirtualBlockIdx start_virtual_idx,
-                                    LogicalBlockIdx start_logical_idx,
-                                    uint8_t num_blocks,
-                                    uint16_t last_remaining) {
-    // prepare the log_entry
-    pmem::LogEntry log_entry;  // NOLINT(cppcoreguidelines-pro-type-member-init)
-    log_entry.op = pmem::LOG_OVERWRITE;
-    log_entry.last_remaining = last_remaining;
-    log_entry.num_blocks = num_blocks;
-    log_entry.next.block_idx = 0;
-    log_entry.next.local_idx = 0;
-    log_entry.start_virtual_idx = start_virtual_idx;
-    log_entry.start_logical_idx = start_logical_idx;
-
-    // check we need to allocate a new log entry block
-    if (local_log_tail.block_idx == 0 ||
-        local_log_tail.local_idx == NUM_LOG_ENTRY - 1) {
-      local_log_tail.block_idx = allocator->alloc(1);
-      local_log_tail.local_idx = 0;
-    }
-
-    // append the log entry
-    auto block = mem_table->get_addr(local_log_tail.block_idx);
-    auto log_entry_block = &block->log_entry_block;
-    log_entry_block->append(log_entry, local_log_tail.local_idx);
-
-    return local_log_tail;
-  }
-
   friend std::ostream& operator<<(std::ostream& out, const TxMgr& tx_mgr) {
     out << "Transaction Log: \n";
 
