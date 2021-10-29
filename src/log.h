d16aa8536fe7ab5bb998bba4578c82a1109e8ae7
Chenhao Ye <ye.chenhao.ych@gmail.com>
2021-10-29 09:55:52 -0500

    add alloc

--- src/log.h
+++ src/log.h
@@ -5,6 +5,8 @@
 #include "alloc.h"
 #include "block.h"
 #include "entry.h"
+#include "idx.h"
+#include "layout.h"
 #include "mtable.h"
 
 namespace ulayfs::dram {
@@ -21,16 +23,22 @@ class LogMgr {
  public:
   LogMgr() = default;
   LogMgr(pmem::MetaBlock* meta, Allocator* allocator, MemTable* mem_table)
-      : meta(meta), allocator(allocator), mem_table(mem_table), free_list() {}
+      : meta(meta), allocator(allocator), mem_table(mem_table), free_list() {
+    free_list.reserve(NUM_LOG_ENTRY);
+  }
 
   pmem::LogEntry* get(pmem::LogEntryIdx idx) {
     return &mem_table->get_addr(idx.block_idx)
                 ->log_entry_block.get(idx.local_idx);
   }
 
+  // TODO: handle linked list
+  // TODO: avoid using the same cacheline for the next op, but use same
+  // cacheline for linked list
   pmem::LogEntryIdx append(pmem::LogOp op, VirtualBlockIdx begin_virtual_idx,
                            LogicalBlockIdx begin_logical_idx,
                            uint8_t num_blocks, uint16_t last_remaining) {
+    if (free_list.empty()) alloc();
     pmem::LogEntryIdx idx = free_list.back();
     free_list.pop_back();
 
@@ -43,5 +51,11 @@ class LogMgr {
 
  private:
   // TODO: allocate LogEntryIdx into from allocator to free_list
+  void alloc() {
+    LogicalBlockIdx idx = allocator->alloc(1);
+    pmem::LogEntryBlock* block = &mem_table->get_addr(idx)->log_entry_block;
+    for (LogLocalIdx i = 0; i < NUM_LOG_ENTRY; ++i)
+      free_list.push_back({idx, i});
+  }
 };
 }  // namespace ulayfs::dram
