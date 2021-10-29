e1133ba4d21bf17f3c8d039d461cc8204c851cc7
Shawn Zhong <github@shawnzhong.com>
2021-10-28 19:59:40 -0500

    Minor changes
    Update block.h

--- src/block.h
+++ src/block.h
@@ -117,10 +117,11 @@ class TxLogBlock : public BaseBlock {
    * Set the next block index
    * @return true on success, false if there is a race condition
    */
-  bool set_next_block_idx(LogicalBlockIdx next) {
+  bool set_next_block_idx(LogicalBlockIdx new_next) {
     LogicalBlockIdx expected = 0;
     bool success = this->next.compare_exchange_strong(
-        expected, next, std::memory_order_release, std::memory_order_acquire);
+        expected, new_next, std::memory_order_release,
+        std::memory_order_acquire);
     persist_cl_fenced(&this->next);
     return success;
   }
