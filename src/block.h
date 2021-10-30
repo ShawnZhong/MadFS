bfce4da872f9b7948430a7918424229f33895e73
Shawn Zhong <github@shawnzhong.com>
2021-10-30 02:12:27 -0500

    Remove useless assert

--- src/block.h
+++ src/block.h
@@ -131,7 +131,6 @@ class LogEntryBlock : public BaseBlock {
 
  public:
   [[nodiscard]] const LogEntry& get(LogLocalIdx idx) {
-    assert(idx >= 0 && idx <= NUM_LOG_ENTRY - 1);
     return log_entries[idx];
   }
 
