2cf4131d795ef96eb1222e31b75d48ecbbe10798
Shawn Zhong <github@shawnzhong.com>
2021-11-01 18:48:13 -0500

    Add write and read back

--- src/lib.cpp
+++ src/lib.cpp
@@ -22,28 +22,42 @@ int open(const char* pathname, int flags, ...) {
   auto file = new dram::File(pathname, flags, mode);
   auto fd = file->get_fd();
   if (file->is_valid()) {
+    debug("ulayfs::open(%s, %x, %x) = %d", pathname, flags, mode, fd);
     files[fd] = file;
   } else {
+    debug("posix::open(%s, %x, %x) = %d", pathname, flags, mode, fd);
     delete file;
   }
-
-  debug("open(%s, %d, %d) = %d", pathname, flags, mode, fd);
 
   return fd;
 }
 
+ssize_t write(int fd, const void* buf, size_t count) {
+  debug("posix::write(%d, buf, %zu)", fd, count);
+  return posix::write(fd, buf, count);
+}
+
+ssize_t read(int fd, void* buf, size_t count) {
+  debug("posix::read(%d, buf, %zu)", fd, count);
+  return posix::read(fd, buf, count);
+}
+
 ssize_t pwrite(int fd, const void* buf, size_t count, off_t offset) {
   if (auto it = files.find(fd); it != files.end()) {
+    debug("ulayfs::pwrite(%d, buf, %zu, %ld)", fd, count, offset);
     return it->second->overwrite(buf, count, offset);
   } else {
+    debug("posix::pwrite(%d, buf, %zu, %ld)", fd, count, offset);
     return posix::pwrite(fd, buf, count, offset);
   }
 }
 
 ssize_t pread(int fd, void* buf, size_t count, off_t offset) {
   if (auto it = files.find(fd); it != files.end()) {
+    debug("ulayfs::pread(%d, buf, %zu, %ld)", fd, count, offset);
     return it->second->pread(buf, count, offset);
   } else {
+    debug("posix::pread(%d, buf, %zu, %ld)", fd, count, offset);
     return posix::pread(fd, buf, count, offset);
   }
 }
