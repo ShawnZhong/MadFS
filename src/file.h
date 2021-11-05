da5cf9c7e80db770290f1b297dd4e9d023a94530
qusuyan <qusuyan@gmail.com>
2021-11-04 19:54:21 -0500

    Remove duplicated macro definitions

--- src/file.h
+++ src/file.h
@@ -15,13 +15,6 @@
 #include "tx.h"
 #include "utils.h"
 
-#define SEEK_SET 0  /* seek relative to beginning of file */
-#define SEEK_CUR 1  /* seek relative to current file position */
-#define SEEK_END 2  /* seek relative to end of file */
-#define SEEK_DATA 3 /* seek to the next data */
-#define SEEK_HOLE 4 /* seek to the next hole */
-#define SEEK_MAX SEEK_HOLE
-
 // data structure under this namespace must be in volatile memory (DRAM)
 namespace ulayfs::dram {
 
