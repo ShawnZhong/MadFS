040570844721170d58e361068f002c658e9e68e6
Shawn Zhong <github@shawnzhong.com>
2021-11-19 14:54:58 -0600

    Fix race condition on tx_idx.local_idx

--- src/tx.h
+++ src/tx.h
@@ -69,7 +69,7 @@ class TxMgr {
                                     pmem::TxBlock*& tx_block,
                                     bool do_alloc) const {
     assert(tx_idx.local_idx >= 0);
-    tx_idx.local_idx++;
+    __atomic_fetch_add(&tx_idx.local_idx, 1, __ATOMIC_ACQ_REL);
     return handle_idx_overflow(tx_idx, tx_block, do_alloc);
   }
 
