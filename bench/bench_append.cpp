05d85d0025790b30ea3c06e5e66edc83c04e510a
Shawn Zhong <github@shawnzhong.com>
2021-12-02 12:26:38 -0600

    Better benchmark output

--- bench/bench_append.cpp
+++ bench/bench_append.cpp
@@ -9,17 +9,15 @@ constexpr const char buf[MAX_SIZE]{};
 int fd;
 
 static void bench(benchmark::State& state) {
-  const auto num_bytes = state.range(0);
-
   if (state.thread_index() == 0) {
     remove(FILEPATH);
     fd = open(FILEPATH, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
   }
 
+  const auto num_bytes = state.range(0);
   for (auto _ : state) {
     ssize_t res = write(fd, buf, num_bytes);
   }
-
   state.SetBytesProcessed(static_cast<int64_t>(state.iterations()) * num_bytes);
 
   if (state.thread_index() == 0) {
