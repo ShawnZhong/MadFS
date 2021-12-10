00d559defad5d8b615d7aa9f6d21adbd1b2ed79c
Chenhao Ye <ye.chenhao.ych@gmail.com>
2021-12-10 09:24:03 -0700

    impl open flags

--- src/file.cpp
+++ src/file.cpp
@@ -5,7 +5,8 @@ namespace ulayfs::dram {
 thread_local std::unordered_map<int, Allocator> File::allocators;
 thread_local std::unordered_map<int, LogMgr> File::log_mgrs;
 
-File::File(int fd, off_t init_file_size, pmem::Bitmap* bitmap, int shm_fd)
+File::File(int fd, off_t init_file_size, pmem::Bitmap* bitmap, int shm_fd,
+           int flags)
     : fd(fd),
       bitmap(bitmap),
       mem_table(fd, init_file_size),
@@ -13,8 +14,11 @@ File::File(int fd, off_t init_file_size, pmem::Bitmap* bitmap, int shm_fd)
       tx_mgr(this, meta),
       blk_table(this, &tx_mgr),
       shm_fd(shm_fd),
-      file_offset(0) {
+      file_offset(0),
+      flags(flags) {
   if (init_file_size == 0) meta->init();
+  // FIXME: the file_offset operation must be thread-safe
+  if (flags & O_APPEND) file_offset += blk_table.get_file_size();
 }
 
 /*
@@ -22,15 +26,49 @@ File::File(int fd, off_t init_file_size, pmem::Bitmap* bitmap, int shm_fd)
  */
 
 ssize_t File::pwrite(const void* buf, size_t count, size_t offset) {
+  if (!(flags & (O_WRONLY | O_RDWR))) {
+    errno = EBADF;
+    return -1;
+  }
   if (count == 0) return 0;
-  tx_mgr.do_write(static_cast<const char*>(buf), count, offset);
-  // TODO: handle write fails i.e. return value != count
-  return static_cast<ssize_t>(count);
+  return tx_mgr.do_write(static_cast<const char*>(buf), count, offset);
+}
+
+ssize_t File::write(const void* buf, size_t count) {
+  if (!(flags & (O_WRONLY | O_RDWR))) {
+    errno = EBADF;
+    return -1;
+  }
+  if (count == 0) return 0;
+  // FIXME: offset upate must is not thread safe (must associated with tx
+  // application on BlkTable)
+  // currently, we always move the offset first so that we could pass the append
+  // test
+  off_t old_offset = __atomic_fetch_add(&file_offset, static_cast<off_t>(count),
+                                        __ATOMIC_ACQ_REL);
+  return tx_mgr.do_write(static_cast<const char*>(buf), count, old_offset);
 }
 
 ssize_t File::pread(void* buf, size_t count, off_t offset) {
+  if (!(flags & (O_RDONLY | O_RDWR))) {
+    errno = EBADF;
+    return -1;
+  }
   if (count == 0) return 0;
   return tx_mgr.do_read(static_cast<char*>(buf), count, offset);
+}
+
+ssize_t File::read(void* buf, size_t count) {
+  if (!(flags & (O_RDONLY | O_RDWR))) {
+    errno = EBADF;
+    return -1;
+  }
+  // FIXME: offset upate must is not thread safe (must associated with tx
+  // application on BlkTable)
+  ssize_t ret = pread(buf, count, file_offset);
+  if (ret > 0)
+    __atomic_fetch_add(&file_offset, static_cast<off_t>(ret), __ATOMIC_ACQ_REL);
+  return ret;
 }
 
 off_t File::lseek(off_t offset, int whence) {
@@ -71,26 +109,6 @@ off_t File::lseek(off_t offset, int whence) {
     default:
       return -1;
   }
-}
-
-ssize_t File::write(const void* buf, size_t count) {
-  // FIXME: offset upate must is not thread safe (must associated with tx
-  // application on BlkTable)
-  // currently, we always move the offset first so that we could pass the append
-  // test
-  off_t old_offset = __atomic_fetch_add(&file_offset, static_cast<off_t>(count),
-                                        __ATOMIC_ACQ_REL);
-  ssize_t ret = pwrite(buf, count, old_offset);
-  return ret;
-}
-
-ssize_t File::read(void* buf, size_t count) {
-  // FIXME: offset upate must is not thread safe (must associated with tx
-  // application on BlkTable)
-  ssize_t ret = pread(buf, count, file_offset);
-  if (ret > 0)
-    __atomic_fetch_add(&file_offset, static_cast<off_t>(ret), __ATOMIC_ACQ_REL);
-  return ret;
 }
 
 int File::fsync() {
