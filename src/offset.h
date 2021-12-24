52e0827a280c19a21e9270f757634e397e40078a
Chenhao Ye <ye.chenhao.ych@gmail.com>
2021-12-24 15:40:37 -0600

    add doc

--- src/offset.h
+++ src/offset.h
@@ -45,20 +45,53 @@ class OffsetMgr {
     return seek_absolute(offset + rel_offset, file_size);
   }
 
-  // must have spinlock acquired
+  /**
+   * move the current offset and get the updated offset; not thread-safe so must
+   * be called with spinlock held; must call release_offset after done
+   *
+   * @param[in] change movement applied to the offset (can be negative)
+   * @param[in] file_size the current file size for boundary check
+   * @param[in] stop_at_boundary whether stop at the boundary
+   * @param[out] ticket ticket for this acquire
+   * @return new offset
+   */
   int64_t acquire_offset(int64_t change, int64_t file_size,
                          bool stop_at_boundary, uint64_t& ticket);
 
-  // thread-safe
+  /**
+   * wait for the previous one to complete; return previous one's idx and block
+   *
+   * @param[in] ticket ticket from acquire_offset
+   * @param[out] prev_idx the previous operation's tx idx (serialization point)
+   * @param[out] prev_block the previous operation's tx block
+   * @return whether need to validate againest prev (if false, prev_idx and
+   * prev_block are undefined)
+   */
   bool wait_offset(uint64_t ticket, TxEntryIdx& prev_idx,
                    const pmem::TxBlock*& prev_block);
 
-  // thread-safe
+  /**
+   * validate whether redo is necessary; the previous operation's serialization
+   * point should be no larger than the current one's
+   *
+   * @param ticket ticket from acquire_offset
+   * @param curr_idx the tx tail idx seen by the current operation
+   * @param curr_block the tx tail block seen by the current operation
+   * @param prev_idx the tx tail idx seen by the previous operation
+   * @param prev_block the tx block idx seen by the previous operation
+   * @return whether the ordering is fine (prev <= curr)
+   */
   bool validate_offset(uint64_t ticket, const TxEntryIdx curr_idx,
                        const pmem::TxBlock* curr_block, TxEntryIdx& prev_idx,
                        const pmem::TxBlock*& prev_block);
 
-  // thread-safe
+  /**
+   * release the offset
+   *
+   * @param ticket ticket from acquire_offset
+   * @param curr_idx the tx tail idx seen by the current operation
+   * @param curr_block the tx tail block seen by the current operation
+   */
   void release_offset(uint64_t ticket, const TxEntryIdx curr_idx,
                       const pmem::TxBlock* curr_block);
 
