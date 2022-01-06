c4af3b85f702771f0721f3063bea84bc60451379
Shawn Zhong <github@shawnzhong.com>
2022-01-06 15:20:16 -0600

    Revert "Remove TxEntryIdx64"
    This reverts commit 00e6d9ae483840b326a65c18d576fff75d62eaac.

--- src/block.h
+++ src/block.h
@@ -138,7 +138,7 @@ class MetaBlock : public BaseBlock {
 
       // hint to find tx log tail; not necessarily up-to-date
       // all tx entries before it must be flushed
-      std::atomic<TxEntryIdx> tx_tail;
+      std::atomic<TxEntryIdx64> tx_tail;
 
       // if inline_tx_entries is used up, this points to the next log block
       std::atomic<LogicalBlockIdx> next_tx_block;
@@ -257,7 +257,7 @@ class MetaBlock : public BaseBlock {
    * @param tx_tail tail value to set
    * @param fenced whether use fence
    */
-  void set_tx_tail(TxEntryIdx tx_tail, bool fenced = false) {
+  void set_tx_tail(TxEntryIdx64 tx_tail, bool fenced = false) {
     cl1_meta.tx_tail.store(tx_tail, std::memory_order_relaxed);
     persist_cl(&cl1, fenced);
   }
@@ -291,7 +291,7 @@ class MetaBlock : public BaseBlock {
     return cl1_meta.next_tx_block.load(std::memory_order_acquire);
   }
   [[nodiscard]] TxEntryIdx get_tx_tail() const {
-    return cl1_meta.tx_tail.load(std::memory_order_relaxed);
+    return cl1_meta.tx_tail.load(std::memory_order_relaxed).tx_entry_idx;
   }
 
   [[nodiscard]] TxEntry get_tx_entry(TxLocalIdx idx) const {
@@ -324,7 +324,8 @@ class MetaBlock : public BaseBlock {
     out << "\tnext_tx_block: "
         << block.cl1_meta.next_tx_block.load(std::memory_order_acquire) << "\n";
     out << "\ttx_tail: "
-        << block.cl1_meta.tx_tail.load(std::memory_order_acquire) << "\n";
+        << block.cl1_meta.tx_tail.load(std::memory_order_acquire).tx_entry_idx
+        << "\n";
     return out;
   }
 };
