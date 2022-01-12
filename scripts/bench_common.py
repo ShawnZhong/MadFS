0f17d0d255a45013a3517710380e999f10a46839
Shawn Zhong <github@shawnzhong.com>
2022-01-12 00:39:05 -0600

    Update bench_common.py
    Update scripts

--- scripts/bench_common.py
+++ scripts/bench_common.py
@@ -9,6 +9,10 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("bench")
 
 
+def drop_cache():
+    system("echo 3 | sudo tee /proc/sys/vm/drop_caches >/dev/null")
+
+
 @dataclass
 class Filesystem:
     name: str
@@ -25,14 +29,19 @@ def get_ext4_dax_path():
     return Path(".")
 
 
-def get_fs_configs():
-    ext4_dax_path = get_ext4_dax_path()
+ext4_dax_path = get_ext4_dax_path()
+_fs_configs = [
+    ("uLayFS", ext4_dax_path, True),
+    ("ext4", ext4_dax_path, False),
+    ("NOVA", "/mnt/pmem0-nova", False),
+]
+fs_names = [fs[0] for fs in _fs_configs]
 
-    for (name, path, load_ulayfs) in [
-        ("uLayFS", ext4_dax_path, True),
-        ("ext4", ext4_dax_path, False),
-        ("NOVA", "/mnt/pmem0-nova", False),
-    ]:
+
+def get_fs_configs(fs_names):
+    for (name, path, load_ulayfs) in _fs_configs:
+        if name not in fs_names:
+            continue
         pmem_path = Path(path)
         if not pmem_path.exists():
             logger.warning(f"{pmem_path} does not exist, skipping {name}")
@@ -40,14 +49,10 @@ def get_fs_configs():
         yield Filesystem(name, pmem_path, load_ulayfs)
 
 
-def drop_cache():
-    system("echo 3 | sudo tee /proc/sys/vm/drop_caches >/dev/null")
-
-
 def add_common_args(argparser):
     argparser.add_argument(
         "-p",
-        dest="result_dir",
+        "--result_dir",
         type=Path,
         help="If set, plot the results in the given directory w/o running the benchmark",
     )
@@ -56,4 +61,12 @@ def add_common_args(argparser):
         "--build_type",
         default="release",
         choices=build_types,
+    )
+    argparser.add_argument(
+        "-f",
+        "--fs_names",
+        default=fs_names,
+        choices=fs_names,
+        nargs="+",
+        help="Filesystems to benchmark",
     )
