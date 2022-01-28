633ce6e83d121c01de47dd632e9a66ae9035163e
Shawn Zhong <github@shawnzhong.com>
2022-01-27 22:35:29 -0600

    Update test_sync.cpp

--- test/test_sync.cpp
+++ test/test_sync.cpp
@@ -6,8 +6,9 @@
 
 #include "common.h"
 
-constexpr auto NUM_BYTES = 128;
-constexpr auto BYTES_PER_THREAD = 2;
+constexpr auto NUM_THREAD = 32;
+constexpr auto NUM_ITER_PER_THREAD = 10;
+constexpr auto BYTES_PER_ITER = 4;
 
 int main() {
   [[maybe_unused]] ssize_t ret;
@@ -15,20 +16,22 @@ int main() {
   unlink(filepath);
   int fd = open(filepath, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
 
-  char empty_buf[NUM_BYTES]{};
-  ret = pwrite(fd, empty_buf, NUM_BYTES, 0);
-  assert(ret == NUM_BYTES);
+  char empty_buf[NUM_THREAD]{};
+  ret = pwrite(fd, empty_buf, NUM_THREAD, 0);
+  assert(ret == NUM_THREAD);
 
   std::vector<std::thread> threads;
 
-  for (int i = 0; i < NUM_BYTES; ++i) {
+  for (int i = 0; i < NUM_THREAD; ++i) {
     threads.emplace_back(std::thread([&, i]() {
-      char buf[BYTES_PER_THREAD]{};
-      fill_buff(buf, BYTES_PER_THREAD, i);
-      ssize_t rc = pwrite(fd, buf, BYTES_PER_THREAD, i);
-      assert(rc == BYTES_PER_THREAD);
-      rc = fsync(fd);
-      assert(rc == 0);
+      char buf[BYTES_PER_ITER]{};
+      fill_buff(buf, BYTES_PER_ITER, i);
+      for (int j = 0; j < NUM_ITER_PER_THREAD; ++j) {
+        ssize_t rc = pwrite(fd, buf, BYTES_PER_ITER, i);
+        assert(rc == BYTES_PER_ITER);
+        rc = fsync(fd);
+        assert(rc == 0);
+      }
     }));
     // uncomment the line below to run sequentially
     //    threads.back().join();
@@ -40,14 +43,14 @@ int main() {
 
   // check result
   {
-    char actual[NUM_BYTES]{};
-    ret = pread(fd, actual, NUM_BYTES, 0);
-    assert(ret == NUM_BYTES);
+    char actual[NUM_THREAD]{};
+    ret = pread(fd, actual, NUM_THREAD, 0);
+    assert(ret == NUM_THREAD);
 
-    char expected[NUM_BYTES];
-    fill_buff(expected, NUM_BYTES);
+    char expected[NUM_THREAD];
+    fill_buff(expected, NUM_THREAD);
 
-    CHECK_RESULT(expected, actual, NUM_BYTES, fd);
+    CHECK_RESULT(expected, actual, NUM_THREAD, fd);
   }
 
   fsync(fd);
