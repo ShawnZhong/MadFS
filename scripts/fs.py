d9dce3aa7de1a2d4235b3baf9acf906574e4614e
Shawn Zhong <github@shawnzhong.com>
2022-06-04 01:38:59 -0500

    Update script

--- scripts/fs.py
+++ scripts/fs.py
@@ -10,7 +10,7 @@ logger = logging.getLogger("fs")
 
 
 def get_ext4_dax_path():
-    for path in ["/mnt/pmem0-ext4-dax", "/mnt/pmem0", "/mnt/pmem1"]:
+    for path in ["/mnt/pmem0-ext4-dax", "/mnt/pmem0", "/mnt/pmem1", "/mnt/pmem_emul"]:
         if Path(path).is_mount():
             return Path(path)
 
@@ -25,6 +25,10 @@ def get_available_filesystems() -> Dict[str, Path]:
         "ext4-dax": ext4_dax_path,
     }
 
+    splitfs_path = Path("/mnt/pmem_emul")
+    if splitfs_path.is_mount():
+        result["SplitFS"] = splitfs_path
+
     nova_path = Path("/mnt/pmem0-nova")
     if nova_path.is_mount():
         result["NOVA"] = nova_path
@@ -34,6 +38,16 @@ def get_available_filesystems() -> Dict[str, Path]:
 
 available_filesystems = get_available_filesystems()
 
+def infer_numa_node(pmem_path):
+    if "pmem" in pmem_path.name:
+        numa_str = pmem_path.name.partition("pmem")[2][0]
+        if numa_str.isnumeric():
+            return int(numa_str)
+
+    logger.warning(f"Cannot infer numa node for {pmem_path}, assuming 0")
+    return 0
+
+
 
 def process_cmd(fs: str, cmd, build_type):
     if fs == "uLayFS" and not is_ulayfs_linked(cmd.split(" ")[0]):
@@ -42,14 +56,23 @@ def process_cmd(fs: str, cmd, build_type):
             logger.warning(f"Cannot find ulayfs path: {ulayfs_path}")
         cmd = f"env LD_PRELOAD={ulayfs_path} {cmd}"
 
+    if fs == "SplitFS":
+        splitfs_path = Path.home() / "SplitFS" / "splitfs"
+        if not splitfs_path.exists():
+            logger.warning(f"Cannot find SplitFS path: {splitfs_path}")
+        cmd = (
+            f"env LD_LIBRARY_PATH={splitfs_path} "
+            f"NVP_TREE_FILE={splitfs_path}/bin/nvp_nvp.tree "
+            f"LD_PRELOAD={splitfs_path}/libnvp.so {cmd}"
+        )
+
     pmem_path = available_filesystems[fs]
     cmd = f"env PMEM_PATH={pmem_path} {cmd}"
 
-    if "pmem" in pmem_path.name:
-        numa = int(pmem_path.name.partition("pmem")[2][0])
-        if shutil.which("numactl"):
-            cmd = f"numactl --cpunodebind={numa} --membind={numa} {cmd}"
-        else:
-            logger.warning("numactl not found, NUMA not enabled")
+    numa = infer_numa_node(pmem_path)
+    if shutil.which("numactl"):
+        cmd = f"numactl --cpunodebind={numa} --membind={numa} {cmd}"
+    else:
+        logger.warning("numactl not found, NUMA not enabled")
 
     return cmd
