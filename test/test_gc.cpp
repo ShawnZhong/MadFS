1d224f9dfed188cd360ad8f807a1504af4adb6a5
Shawn Zhong <github@shawnzhong.com>
2022-09-13 07:52:47 -0500

    Add sync test for gc

--- test/test_gc.cpp
+++ test/test_gc.cpp
@@ -14,7 +14,7 @@ using ulayfs::debug::clear_count;
 using ulayfs::debug::print_file;
 using ulayfs::utility::GarbageCollector;
 
-struct TestOpt {
+struct BasicTestOpt {
   int num_bytes_per_iter = BLOCK_SIZE;
   int num_iter = NUM_INLINE_TX_ENTRY + NUM_TX_ENTRY_PER_BLOCK + 1;
   bool print = false;
@@ -23,9 +23,8 @@ struct TestOpt {
   int random_block_range = 0;
 };
 
-void test(TestOpt test_opt) {
-  const auto& [num_bytes_per_iter, num_iter, print, random_block_range] =
-      test_opt;
+void basic_test(BasicTestOpt opt) {
+  const auto& [num_bytes_per_iter, num_iter, print, random_block_range] = opt;
 
   unlink(filepath);
   int fd = open(filepath, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
@@ -65,20 +64,83 @@ void test(TestOpt test_opt) {
   }
 }
 
+void sync_test() {
+  unlink(filepath);
+  int fd = open(filepath, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+
+  std::mutex mutex;
+  std::condition_variable cv;
+
+  int num_io_threads = 8;
+  bool gc_done = false;
+  std::vector io_threads_done(num_io_threads, false);
+  std::vector<std::thread> io_threads;
+
+  for (int i = 0; i < num_io_threads; ++i) {
+    io_threads.emplace_back([&, i]() {
+      std::unique_lock<std::mutex> lock{mutex};
+      if (i != 0) {
+        // wait for the previous thread
+        cv.wait(lock, [&]() { return io_threads_done[i - 1]; });
+      }
+
+      // the first thread writes NUM_INLINE_TX_ENTRY + 1 blocks
+      int num_iter = i == 0 ? NUM_INLINE_TX_ENTRY + 1 : NUM_TX_ENTRY_PER_BLOCK;
+
+      LOG_INFO("thread %d start", i);
+      char buf[BLOCK_SIZE]{};
+      for (int iter = 0; iter < num_iter; ++iter) {
+        auto ret = pwrite(fd, buf, BLOCK_SIZE, 0);
+        ASSERT(ret == BLOCK_SIZE);
+      }
+      fsync(fd);
+      LOG_INFO("thread %d finished writing", i);
+
+      // notify the next thread to start
+      io_threads_done[i] = true;
+      cv.notify_all();
+
+      // keep running until the gc is done
+      cv.wait(lock, [&]() { return gc_done; });
+      LOG_INFO("thread %d exiting", i);
+    });
+  }
+
+  std::thread gc([&]() {
+    std::unique_lock<std::mutex> lock{mutex};
+    cv.wait(lock, [&]() { return io_threads_done[num_io_threads - 1]; });
+
+    LOG_INFO("gc start");
+
+    GarbageCollector garbage_collector(filepath);
+    std::cerr << garbage_collector.file->shm_mgr;
+    garbage_collector.gc();
+
+    LOG_INFO("gc finished");
+
+    gc_done = true;
+    cv.notify_all();
+  });
+
+  gc.join();
+  for (auto& t : io_threads) t.join();
+}
+
 int main() {
   srand(0);  // NOLINT(cert-msc51-cpp)
 
-  //  test({1, 1'000'000});
-  //  test({BLOCK_SIZE * 64, 1'000'000});
-  test({.num_bytes_per_iter = BLOCK_SIZE,
-        .num_iter = NUM_INLINE_TX_ENTRY + NUM_TX_ENTRY_PER_BLOCK + 1,
-        .print = true,
-        .random_block_range = 1000});
-  test({BLOCK_SIZE});
+  //  basic_test({1, 1'000'000});
+  //  basic_test({BLOCK_SIZE * 64, 1'000'000});
+
+  basic_test({});
+  basic_test({.random_block_range = 1000});
+
   if constexpr (!ulayfs::BuildOptions::use_pmemcheck) {
     // the following tests are too slow for pmemcheck
-    test({BLOCK_SIZE * 63});
-    test({BLOCK_SIZE * 64});
-    test({BLOCK_SIZE * 65});
+    basic_test({.num_bytes_per_iter = BLOCK_SIZE * 63});
+    basic_test({.num_bytes_per_iter = BLOCK_SIZE * 64});
+    basic_test({.num_bytes_per_iter = BLOCK_SIZE * 65});
   }
+
+  sync_test();
 }
