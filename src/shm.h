e11a42cba20f172b869e56687a555feadcc18272
Shawn Zhong <github@shawnzhong.com>
2022-09-13 23:52:20 -0500

    Add TODO

--- src/shm.h
+++ src/shm.h
@@ -65,7 +65,8 @@ class alignas(SHM_PER_THREAD_SIZE) PerThreadData {
     index = i;
     tx_block_idx.store(0, std::memory_order_relaxed);
     init_robust_mutex(&mutex);
-    pthread_mutex_lock(&mutex);
+    // TODO: uncomment this
+    //    pthread_mutex_lock(&mutex);
     state.store(State::INITIALIZED, std::memory_order_release);
 
     LOG_DEBUG("PerThreadData %ld initialized by tid %d", i, tid);
@@ -78,7 +79,8 @@ class alignas(SHM_PER_THREAD_SIZE) PerThreadData {
   void reset() {
     LOG_DEBUG("PerThreadData %ld to be reset by tid %d", index, tid);
     assert(state.load(std::memory_order_acquire) == State::INITIALIZED);
-    if (is_thread_alive()) pthread_mutex_unlock(&mutex);
+    // TODO: uncomment this
+    //    if (is_thread_alive()) pthread_mutex_unlock(&mutex);
     state.store(State::PENDING, std::memory_order_acq_rel);
     index = 0;
     tx_block_idx.store(0, std::memory_order_relaxed);
